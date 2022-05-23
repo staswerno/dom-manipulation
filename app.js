0e75ce6d84e7d2fb06a495c8871285af1faed603
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-05-23 10:47:31 +0200

    add sibling traversal

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-// traversing the dom - to parents and children
-
 const bookList = document.querySelector("#book-list");
 
-// ususally will return the same element
-// can chan to go up levels
+// includes line break
+console.log("book-list next sibling is:", bookList.nextSibling);
 
-console.log("the parent node is: ", bookList.parentNode);
-console.log("the parent node is: ", bookList.parentElement);
-console.log("the parent node is: ", bookList.parentElement.parentElement);
+// doesn't include line break
+console.log("book-list next element sibling is:", bookList.nextElementSibling);
 
-// text in console are linebreaks so...
-console.log(bookList.childNodes);
+// includes line break
+console.log("book-list next sibling is:", bookList.previousSibling);
 
-// ...use this instead
-console.log(bookList.children);
+// doesn't include line break
+console.log(
+	"book-list next element sibling is:",
+	bookList.previousElementSibling
+);
+
+bookList.previousElementSibling.querySelector("p").innerHTML +=
+	"<br />too cool for not knights";
