86589ba81cc80643385962d053fcbba043bfd51d
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-05-23 10:40:43 +0200

    add dom traversal parents and children

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
-const books = document.querySelectorAll("#book-list li .name");
-
-// note textContent property
-// += appends instead of changes
-books.forEach((book) => {
-	book.textContent += " (book title)";
-});
+// traversing the dom - to parents and children
 
 const bookList = document.querySelector("#book-list");
-// bookList.innerHTML = "<h2>BOOKS BOOKS BOOKS</h2>";
-bookList.innerHTML += "<p>this iz how add htlm</p>";
-console.log(bookList.innerHTML);
 
-const banner = document.querySelector("#page-banner");
-console.log(`#page-banner node type is ${banner.nodeType}`);
-console.log(`#page-banner node name is ${banner.nodeName}`);
-console.log(`#page-banner has child nodes? ${banner.hasChildNodes()}`);
+// ususally will return the same element
+// can chan to go up levels
 
-// clone a node
-// without passing true, does not include nested content
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
+console.log("the parent node is: ", bookList.parentNode);
+console.log("the parent node is: ", bookList.parentElement);
+console.log("the parent node is: ", bookList.parentElement.parentElement);
+
+// text in console are linebreaks so...
+console.log(bookList.childNodes);
+
+// ...use this instead
+console.log(bookList.children);
--- jsnotes.js
+++ jsnotes.js
@@ -23,3 +23,26 @@ console.log(books);
 // see arraynotes.js for Array.from info
 // querySelectorAll actually returns a Node List which CAN use forEach method
 books.forEach((book) => console.log(book));
+
+const books = document.querySelectorAll("#book-list li .name");
+
+// note textContent property
+// += appends instead of changes
+books.forEach((book) => {
+	book.textContent += " (book title)";
+});
+
+const bookList = document.querySelector("#book-list");
+// bookList.innerHTML = "<h2>BOOKS BOOKS BOOKS</h2>";
+bookList.innerHTML += "<p>this iz how add htlm</p>";
+console.log(bookList.innerHTML);
+
+const banner = document.querySelector("#page-banner");
+console.log(`#page-banner node type is ${banner.nodeType}`);
+console.log(`#page-banner node name is ${banner.nodeName}`);
+console.log(`#page-banner has child nodes? ${banner.hasChildNodes()}`);
+
+// clone a node
+// without passing true, does not include nested content
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
