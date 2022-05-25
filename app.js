469e1b396ccd5d6b90c5b076759f7cbc4b434b2e
Staś Werno <89650713+staswerno@users.noreply.github.com>
2022-05-25 09:49:27 +0200

    create elements

--- app.js
+++ app.js
@@ -137,4 +137,19 @@ addForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const value = addForm.querySelector('input[type="text"]').value;
 	console.log(value);
+
+	// create elements
+	const li = document.createElement("li");
+	const bookName = document.createElement("span");
+	const deleteBtn = document.createElement("span");
+
+	// add content
+
+	deleteBtn.textContent = "delete";
+	bookName.textContent = value;
+
+	// append to document
+	li.appendChild(bookName);
+	li.appendChild(deleteBtn);
+	list.appendChild(li);
 });
